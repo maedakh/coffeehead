14a47b60d0f107a31aa4c99ca605f1877d78423e
AM <ahmad.mustofa71@gmail.com>
2021-02-06 00:01:14 +0700

    logic tampil data

--- app/src/main/java/com/example/coffeehead/MainActivity.kt
+++ app/src/main/java/com/example/coffeehead/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.coffeehead
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var listCoffeeAdapter: ListCoffeeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listCoffeeAdapter = ListCoffeeAdapter(CoffeeData.listData)
+        rv_coffee.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = listCoffeeAdapter
+        }
+
+        listCoffeeAdapter.setOnItemClickCallback(object : ListCoffeeAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Coffee) {
+                Toast.makeText(this@MainActivity, data.toString(), Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
