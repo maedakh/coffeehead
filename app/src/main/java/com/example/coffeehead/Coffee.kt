5577f2ce5bbe4c8937b4533ba440c66a95745518
AM <ahmad.mustofa71@gmail.com>
2021-02-05 23:31:34 +0700

    bikin model jadi support parcelable

--- app/src/main/java/com/example/coffeehead/Coffee.kt
+++ app/src/main/java/com/example/coffeehead/Coffee.kt
@@ -1,7 +1,11 @@
 package com.example.coffeehead
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Coffee (
     var name: String = "",
     var detail: String = "",
     var photo: Int = 0
-)
+):Parcelable
