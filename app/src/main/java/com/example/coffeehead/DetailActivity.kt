ae695c4f97546af32cf428c2f3b837e05b20f031
AM <ahmad.mustofa71@gmail.com>
2021-02-17 18:10:48 +0700

    tampilkan data kiriman dari main activity

--- app/src/main/java/com/example/coffeehead/DetailActivity.kt
+++ app/src/main/java/com/example/coffeehead/DetailActivity.kt
@@ -2,10 +2,41 @@ package com.example.coffeehead
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_DETAIL = "extra_detail"
+    }
+
+    private var dataCoffee = Coffee()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val supportActionBar = supportActionBar
+
+        if (intent.extras != null) {
+            dataCoffee = intent.getParcelableExtra(EXTRA_DETAIL)!!
+
+            //Set nama toolbar sesuai nama kopi
+            supportActionBar?.title = dataCoffee.name
+
+            showDataToView(dataCoffee)
+        }
+    }
+
+    private fun showDataToView(data: Coffee) {
+        data.let {
+            tvTitle.text = it.name
+            tvDetail.text = it.detail
+
+            Glide.with(this)
+                .load(it.photo)
+                .centerCrop()
+                .into(ivCoffee)
+        }
     }
 }
